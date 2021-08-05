c8bf9c164eaa11f06f5f2404d6606b3e65820ea3
rhyrms <rhyrms@hotmail.com>
2021-08-05 00:42:33 -0300

    Finalizado o módulo 16, onde foram implementados forms no projeto principal do curso.

---
+++ the-complete-guide/course-project/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -0,0 +1,61 @@
+import { Subscription } from 'rxjs';
+import { Component, OnInit, OnDestroy, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Ingredient } from 'src/app/shared/ingredient.model';
+import { ShoppingListService } from '../shopping-list.service';
+
+@Component({
+  selector: 'app-shopping-edit',
+  templateUrl: './shopping-edit.component.html',
+  styleUrls: ['./shopping-edit.component.css']
+})
+export class ShoppingEditComponent implements OnInit, OnDestroy {
+  @ViewChild('f') slForm: NgForm;
+  subscription: Subscription;
+  editMode: boolean = false;
+  editedItemIndex: number;
+  editedItem: Ingredient;
+
+  constructor(private slService: ShoppingListService) { }
+
+  ngOnInit(): void {
+    this.subscription = this.slService.startedEditing
+      .subscribe((index: number) => {
+        this.editMode = true;
+        this.editedItemIndex = index;
+        this.editedItem = this.slService.getIngredient(index);
+        this.slForm.setValue({
+          name: this.editedItem.name,
+          amount: this.editedItem.amount
+        });
+      });
+  }
+
+  onSubmit(form: NgForm) {
+    const value = form.value
+    const newIngredient: Ingredient = new Ingredient(value.name, value.amount);
+
+    if (this.editMode == true) {
+      this.slService.updateIngredient(this.editedItemIndex, newIngredient);
+    } else {
+      this.slService.addIngredient(newIngredient);
+    }
+    // this.editMode = false;
+    // form.reset();
+    this.onClear();
+  }
+
+  onClear() {
+    this.slForm.reset();
+    this.editMode = false;
+  }
+
+  onDelete() {
+    this.onClear();
+    this.slService.deleteIngredient(this.editedItemIndex);
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
+  }
+}
---
+++ the-complete-guide/course-project/src/app/shopping-list/shopping-list.service.ts
@@ -0,0 +1,52 @@
+import { Injectable, EventEmitter } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
+import { Subject } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ShoppingListService {
+  // ingredientsChanged: EventEmitter<Ingredient[]> = new EventEmitter<Ingredient[]>();
+  ingredientsChanged: Subject<Ingredient[]> = new Subject<Ingredient[]>();
+  startedEditing: Subject<number> = new Subject<number>();
+  private ingredients: Ingredient[] = [
+    new Ingredient('Apples', 4),
+    new Ingredient('Tomatoes', 17),
+    new Ingredient('Potatoes', 2)
+  ];
+
+  constructor() { }
+
+  getIngredients(): Ingredient[] {
+    return this.ingredients.slice();
+  }
+
+  addIngredient(ingredient: Ingredient): void {
+    this.ingredients.push(ingredient);
+    // this.ingredientsChanged.emit(this.ingredients.slice());
+    this.ingredientsChanged.next(this.ingredients.slice());
+  }
+
+  addIngredients(ingredients: Ingredient[]) {
+    // for (let ingredient of ingredients) {
+    //   this.addIngredient(ingredient);
+    // }
+    this.ingredients.push(...ingredients);
+    // this.ingredientsChanged.emit(this.ingredients.slice());
+    this.ingredientsChanged.next(this.ingredients.slice());
+  }
+
+  getIngredient(index: number) {
+    return this.ingredients[index];
+  }
+
+  updateIngredient(index: number, newIngredient: Ingredient) {
+    this.ingredients[index] = newIngredient;
+    this.ingredientsChanged.next(this.ingredients.slice());
+  }
+
+  deleteIngredient(index: number) {
+    this.ingredients.splice(index, 1);
+    this.ingredientsChanged.next(this.ingredients.slice());
+  }
+}
