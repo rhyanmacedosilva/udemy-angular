4bfe0f89382bd1a34a5f3de9195e6e5ad4a907f9
rhyrms <rhyrms@hotmail.com>
2021-08-10 01:00:56 -0300

    Iniciado módulo referente ao HttpClient.

---
+++ the-complete-guide/http-01-start/src/app/app.component.ts
@@ -0,0 +1,56 @@
+import { Post } from './post';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+  loadedPosts: Post[] = [];
+  isFetching: boolean = false;
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit() {
+    this.fetchPosts();
+  }
+
+  onCreatePost(postData: { title: string; content: string }) {
+    // Send Http request
+    this.http.post<{ name: string }>(
+      'https://angular-complete-guide-43a49-default-rtdb.firebaseio.com/posts.json',
+      postData).subscribe((responseData) => {
+        console.log(responseData);
+      });
+  }
+
+  onFetchPosts() {
+    // Send Http request
+    this.fetchPosts();
+  }
+
+  onClearPosts() {
+    // Send Http request
+  }
+
+  private fetchPosts() {
+    this.isFetching = true;
+    this.http.get<{ [key: string]: Post }>('https://angular-complete-guide-43a49-default-rtdb.firebaseio.com/posts.json')
+      .pipe(map((responseData) => {
+        const postsArray: Post[] = [];
+        for (const key in responseData) {
+          if (responseData.hasOwnProperty(key) == true) {
+            postsArray.push({ ...responseData[key], id: key });
+          }
+        }
+        return postsArray;
+      }))
+      .subscribe((responseData) => {
+        this.isFetching = false;
+        this.loadedPosts = responseData;
+      });
+  }
+}
